6dbf60613ab5211518ebbf30e9c6bf1188374646
nobodys5 <155369610+nobodys5@users.noreply.github.com>
2024-05-08 19:30:16 +0900

    Add files via upload

---
+++ new-web-project/src/main/java/com/example/demo/NewWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWebProjectApplication.class, args);
+	}
+
+}
